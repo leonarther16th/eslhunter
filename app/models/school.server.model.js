821ee8db90aef83d8aaf89a2a5f4158b281ede07
Moe Katib <Moe.Katib@gmail.com>
2015-02-23 13:57:39 -0500

    added school crud

---
+++ app/models/school.server.model.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * School Schema
+ */
+var SchoolSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill School name',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('School', SchoolSchema);
